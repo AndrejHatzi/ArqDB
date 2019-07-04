b4e07d10dd21e94d08ea550244d4546d923674bf
Andrej Hatzi <45495944+AndrejHatzi@users.noreply.github.com>
2019-07-04 19:33:04 +0100

    Add files via upload

--- ArqDB/My Project/Resources.Designer.vb
+++ ArqDB/My Project/Resources.Designer.vb
@@ -59,5 +59,25 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property oasis() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("oasis", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property pyramide_render() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("pyramide_render", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace
